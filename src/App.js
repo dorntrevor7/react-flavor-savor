5ce294059d08086380f055b250899a0bb7aed92f
Trevor Dorn <dorntrevor7@gmail.com>
2020-06-23 07:08:50 -0700

    api working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Recipe from "./components/recipe";
+import "./App.css";
 
 function App() {
+  const recipeID = "43295c4f";
+  const recipeKEY = "8ec387abba983b997fa25e14be336ccc";
+  const [recipe, setRecipes] = useState([]);
+  const recipeURL = `https://api.edamam.com/search?app_key=
+    ${recipeKEY}
+    &app_id=
+    ${recipeID}
+    &q=recipe
+    &to=18`;
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(recipeURL);
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <p>Recipe List</p>
+        <form></form>
+        {recipe.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
